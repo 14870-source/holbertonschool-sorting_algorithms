607069d9c005119a1a35d95a5c1e2c648283171b
Sultanjafari <Sultan06668@gmail.com>
2026-08-22 17:03:49 +0000

    Add Selection Sort implementation and Big O notations

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,26 +8,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
-	int tmp;
+	size_t i, j, jMin;
+	int temp;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_idx = i;
+		jMin = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
+			if (array[j] < array[jMin])
+			{
+				jMin = j;
+			}
 		}
 
-		if (min_idx != i)
+		if (jMin != i)
 		{
-			tmp = array[i];
-			array[i] = array[min_idx];
-			array[min_idx] = tmp;
+			temp = array[i];
+			array[i] = array[jMin];
+			array[jMin] = temp;
 			print_array(array, size);
 		}
 	}
